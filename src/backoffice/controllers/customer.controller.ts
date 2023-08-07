1d8b480701bd55eb7f1ad51289095f0ae2a515d8
isadorabrito <isadoradpbrito@gmail.com>
2023-08-07 14:09:57 -0300

    feat: add routes to customer controller

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
-@Controller()
+@Controller('v1/customers')
 export class CustomerController {
+  @Get()
   get() {
-    return 'Clients list';
+    return 'get clients list';
+  }
+  @Post()
+  post() {
+    return 'create a new client';
+  }
+  @Put()
+  put() {
+    return 'update a client';
+  }
+  @Delete()
+  delete() {
+    return 'delete a client';
   }
 }
