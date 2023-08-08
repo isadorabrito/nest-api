526d0b178184e80f417fc75c188bc2c148cf7a8a
isadorabrito <isadoradpbrito@gmail.com>
2023-08-08 01:00:14 -0300

    feat: add type to customer model and result module

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -7,29 +7,31 @@ import {
   Param,
   Body,
 } from '@nestjs/common';
+import { Customer } from '../models/customer.model';
+import { Result } from '../models/result.model';
 
 @Controller('v1/customers')
 export class CustomerController {
   @Get()
   get() {
-    return 'get clients list';
+    return new Result(null, true, [], null);
   }
 
   @Get(':id')
   getBtId(@Param('id') id) {
-    return 'get clients list' + id;
+    return new Result(null, true, {}, null);
   }
 
   @Post()
-  post(@Body() body) {
-    return body;
+  post(@Body() body: Customer) {
+    return new Result('client created!', true, body, null);
   }
   @Put(':id')
   put(@Param('id') id, @Body() body) {
-    return { customer: id, data: body };
+    return new Result('client updated!', true, body, null);
   }
   @Delete(':id')
   delete(@Param('id') id) {
-    return 'client ' + id + ' deleted';
+    return new Result('client deleted!', true, null, null);
   }
 }
