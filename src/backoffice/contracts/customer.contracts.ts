22759bbcefaa53edba079eacdbbc235cf2534b80
isadorabrito <isadoradpbrito@gmail.com>
2023-08-09 13:23:19 -0300

    feat: add create-customer dto to customer contract and controller

--- src/backoffice/contracts/customer.contracts.ts
+++ src/backoffice/contracts/customer.contracts.ts
@@ -1,17 +1,18 @@
 import { Flunt } from 'src/utils/flunt';
 import { Contract } from './contract';
-import { Customer } from '../models/customer.model';
 import { Injectable } from '@nestjs/common';
+import { CreateCustomerDto } from '../dtos/create-customer';
 
 @Injectable()
 export class CreateCustomerContract implements Contract {
   errors: any[];
-  validate(model: Customer): boolean {
+  validate(model: CreateCustomerDto): boolean {
     const flunt = new Flunt();
 
     flunt.hasMinLen(model.name, 5, 'Invalid name!');
     flunt.isEmail(model.email, 'Invalid mail!');
     flunt.isFixedLen(model.document, 11, 'Invalid CPF!');
+    flunt.hasMinLen(model.password, 6, 'Invalid password!');
 
     this.errors = flunt.errors;
     return flunt.isValid();
